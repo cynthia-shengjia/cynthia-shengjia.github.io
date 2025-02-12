7118e85f35fd53d2adff3a196df184c5ffecaf59
cynthia-shengjia <1455319292@qq.com>
2025-02-12 16:07:22 +0800

    test-7

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,6 @@
 # for file in images/*.{jpg,png}; do
 #     [ ! -f "tn/$file" ] && magick "$file"  -thumbnail 160x160 "tn/$file"
 # done
+
+
+ magick ./images/avatar.jpg  -thumbnail 160x160 ./tn/images/avatar.jpg
